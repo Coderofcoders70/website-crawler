d8d27087bf46f6f7db6137e4ebf47946f7673c1c
Coderofcoders70 <Lakshayas918@gmail.com>
2025-02-16 20:02:28 +0530

    Crawling is working successfully

--- crawl.js
+++ crawl.js
@@ -1,5 +1,46 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if(baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+    }   
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if(pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+    
+    console.log(`Successfully Crawling: ${currentURL}`);
+    try{
+        const resp = await fetch(currentURL);
+        if(resp.status > 399){
+            console.log(`Error in fetching with status code: ${resp.status} on page: ${currentURL}`);
+            return pages
+        }
+
+        const contentType = resp.headers.get("Content-Type")
+        if(!contentType.includes("text/html")){
+            console.log(`No Html response, Content Type: ${contentType} on page: ${currentURL}`);
+            return pages
+        }
+
+        const htmlBody = await resp.text();
+
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for(const nextURL of nextURLs){
+            pages = await crawlPage(baseURL, nextURL, pages )
+        }
+    }catch(err){
+        console.log(`Error in fetching the data: ${err.message}, on page: ${currentURL}`);
+    }
+    return pages
+}
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -38,5 +79,6 @@ function normalizeURL(urlString){
 
 module.exports ={
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const {crawlPage} =  require('./crawl.js')
+
+async function main(){
+    if(process.argv.length < 3){
+        console.log("No Website Provided")
+        process.exit(1)
+    }
+    if(process.argv.length > 3){
+        console.log("Too many CLI")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2]
+
+    console.log(`Starting crawl for ${baseURL}`);
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    for(const page of Object.entries(pages)){
+        console.log(page)
+    }
+}
+
+main()
